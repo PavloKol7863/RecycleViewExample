3401a16f3d2a1f3b338329cb58eae19e8d609f15
Pavlo Koldun <pavlo123koldun@gmail.com>
2020-12-21 18:49:13 +0200

    Created toolbar

--- app/src/main/java/ua/application/recycleviewexample/MainActivity.java
+++ app/src/main/java/ua/application/recycleviewexample/MainActivity.java
@@ -1,6 +1,7 @@
 package ua.application.recycleviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
@@ -8,10 +9,15 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     FragmentTransaction ft;
     FragmentRecycle fragmentRecycle;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
 
         if (savedInstanceState == null) {
             ft = getSupportFragmentManager().beginTransaction();
